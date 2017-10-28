91467642455cc68d8bdcaf2112971b933549874e
Ricardo Olivieri <roliv@us.ibm.com>
2017-10-28 15:25:08 -0500

    migration to swift 4

--- Package.swift
+++ Package.swift
@@ -1,3 +1,6 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 /**
  * Copyright IBM Corporation 2016, 2017
  *
@@ -18,7 +21,22 @@ import PackageDescription
 
 let package = Package(
     name: "Kitura-CredentialsGoogle",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CredentialsGoogle",
+            targets: ["CredentialsGoogle"]
+        )
+    ],    
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 7),
+         .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", .upToNextMinor(from: "2.0.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CredentialsGoogle",
+            dependencies: ["Credentials"]
+        )
     ]
 )
