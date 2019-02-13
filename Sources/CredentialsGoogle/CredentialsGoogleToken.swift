6d3128d7c3eab5d980e36ef4b920fdd346912fec
andy <andrew.lees11@ibm.com>
2019-02-13 12:04:26 +0000

    Using Credentials 2.4 for ttl

--- Sources/CredentialsGoogle/CredentialsGoogleToken.swift
+++ Sources/CredentialsGoogle/CredentialsGoogleToken.swift
@@ -36,7 +36,7 @@ public class CredentialsGoogleToken: CredentialsPluginProtocol {
     }
     
     /// The time in seconds since the user profile was generated that the access token will be considered valid.
-    public var tokenTimeToLive: TimeInterval?
+    public let tokenTimeToLive: TimeInterval?
 
     private var delegate: UserProfileDelegate?
     
@@ -48,8 +48,9 @@ public class CredentialsGoogleToken: CredentialsPluginProtocol {
     /// Initialize a `CredentialsGoogleToken` instance.
     ///
     /// - Parameter options: A dictionary of plugin specific options. The keys are defined in `CredentialsGoogleOptions`.
-    public init(options: [String:Any]?=nil) {
+    public init(options: [String:Any]?=nil, tokenTimeToLive: TimeInterval? = nil) {
         delegate = options?[CredentialsGoogleOptions.userProfileDelegate] as? UserProfileDelegate
+        self.tokenTimeToLive = tokenTimeToLive
     }
     
     /// User profile cache.
@@ -86,8 +87,10 @@ public class CredentialsGoogleToken: CredentialsPluginProtocol {
                             onSuccess(cached.userProfile)
                             return
                         }
-                    // If no time to live set use token until it is evicted from the cache
+                        // If current time is later than time to live, continue to standard token authentication.
+                        // Don't need to evict token, since it will replaced if the token is successfully autheticated.
                     } else {
+                        // No time to live set, use token until it is evicted from the cache
                         onSuccess(cached.userProfile)
                         return
                     }
